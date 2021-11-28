6edb1c9ff35a2f4d6fbd8ef3cfe1800d9410ecc3
kostarter <49716175+kostarter@users.noreply.github.com>
2021-11-28 18:29:56 +0100

    Update s3-to-redshift.sql

--- power-bi-with-amazon-redshift/s3-to-redshift.sql
+++ power-bi-with-amazon-redshift/s3-to-redshift.sql
@@ -1,34 +1,34 @@
 COPY public.customer 
 FROM 's3://m2i-redshift-sample/tpch-data/customer/'
-CREDENTIALS 'aws_access_key_id=AKIAXS5AU5FGSPCQI76P;aws_secret_access_key=Ci0/vlAxxJ/hEEVfjRMhV1mKJ8TiCwiyTPS20Ww2';
+CREDENTIALS 'aws_access_key_id=<aws_access_key>;aws_secret_access_key=<aws_secret_access_key>';
 
 COPY public.nation
 FROM 's3://m2i-redshift-sample/tpch-data/nation/'
-CREDENTIALS 'aws_access_key_id=AKIAXS5AU5FGSPCQI76P;aws_secret_access_key=Ci0/vlAxxJ/hEEVfjRMhV1mKJ8TiCwiyTPS20Ww2';
+CREDENTIALS 'aws_access_key_id=<aws_access_key>;aws_secret_access_key=<aws_secret_access_key>';
 
 COPY public.part
 FROM 's3://m2i-redshift-sample/tpch-data/part/'
-CREDENTIALS 'aws_access_key_id=AKIAXS5AU5FGSPCQI76P;aws_secret_access_key=Ci0/vlAxxJ/hEEVfjRMhV1mKJ8TiCwiyTPS20Ww2';
+CREDENTIALS 'aws_access_key_id=<aws_access_key>;aws_secret_access_key=<aws_secret_access_key>';
 
 COPY public.partsupp
 FROM 's3://m2i-redshift-sample/tpch-data/partsupp/'
-CREDENTIALS 'aws_access_key_id=AKIAXS5AU5FGSPCQI76P;aws_secret_access_key=Ci0/vlAxxJ/hEEVfjRMhV1mKJ8TiCwiyTPS20Ww2';
+CREDENTIALS 'aws_access_key_id=<aws_access_key>;aws_secret_access_key=<aws_secret_access_key>';
 
 COPY public.region
 FROM 's3://m2i-redshift-sample/tpch-data/region/'
-CREDENTIALS 'aws_access_key_id=AKIAXS5AU5FGSPCQI76P;aws_secret_access_key=Ci0/vlAxxJ/hEEVfjRMhV1mKJ8TiCwiyTPS20Ww2';
+CREDENTIALS 'aws_access_key_id=<aws_access_key>;aws_secret_access_key=<aws_secret_access_key>';
 
 COPY public.supplier
 FROM 's3://m2i-redshift-sample/tpch-data/supplier/'
-CREDENTIALS 'aws_access_key_id=AKIAXS5AU5FGSPCQI76P;aws_secret_access_key=Ci0/vlAxxJ/hEEVfjRMhV1mKJ8TiCwiyTPS20Ww2';
+CREDENTIALS 'aws_access_key_id=<aws_access_key>;aws_secret_access_key=<aws_secret_access_key>';
 
 COPY public.orders
 FROM 's3://m2i-redshift-sample/tpch-data/orders/'
-CREDENTIALS 'aws_access_key_id=AKIAXS5AU5FGSPCQI76P;aws_secret_access_key=Ci0/vlAxxJ/hEEVfjRMhV1mKJ8TiCwiyTPS20Ww2';
+CREDENTIALS 'aws_access_key_id=<aws_access_key>;aws_secret_access_key=<aws_secret_access_key>';
 
 COPY public.lineitem 
 FROM 's3://m2i-redshift-sample/tpch-data/lineitem/'
-CREDENTIALS 'aws_access_key_id=AKIAXS5AU5FGSPCQI76P;aws_secret_access_key=Ci0/vlAxxJ/hEEVfjRMhV1mKJ8TiCwiyTPS20Ww2';
+CREDENTIALS 'aws_access_key_id=<aws_access_key>;aws_secret_access_key=<aws_secret_access_key>';
 
 SELECT COUNT(*) FROM public.customer; 
 SELECT COUNT(*) FROM public.nation;
